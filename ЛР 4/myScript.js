623e1b216365e585d9b2f8075912a3bc09d01efc
qwe3qwe3lott <qwe3qwe3lott@gmail.com>
2021-03-29 11:03:28 +0300

    l5 l6 l7 l8

---
+++ ЛР 4/myScript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#menu').hide();
+    $('#menu__exit').click(function(){
+        $('#menu').hide();
+    });
+    $('#menu-button').click(function(){
+        $('#menu').show();
+    });
+    $('#form').hide();
+    $('#form__exit').click(function(){
+        $('#form').hide();
+    });
+    $('#promotions__button').click(function(){
+        $('#form').show();
+    });
+});
